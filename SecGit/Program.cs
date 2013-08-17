dfb5e2c2a164032809143148f5784b31f3d3fa21
Saad Rih <saadrih@index-dev.net>
2013-08-17 08:19:53 +0000

    Changed the Dev BR

--- SecGit/Program.cs
+++ SecGit/Program.cs
@@ -17,6 +17,7 @@ namespace SecGit
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            //Changed in Dev
         }
     }
 }
